c0f5d01e539038e132011df8e021a824ac097f70
RahulPatnaik <142738799+RahulPatnaik@users.noreply.github.com>
2023-12-30 15:42:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -7,30 +7,22 @@ import os
 
 print(os.getcwd())
 
-# Determine the full path to the directory 
 current_directory = os.path.dirname(os.path.abspath(__file__))
 
-# Load the trained machine learning model
 model_path = os.path.join(current_directory, 'biocompatibility_model.pkl')
 model = joblib.load(model_path)
 
 
-# load and preprocess the data
 def load_data(file_path):
     try:
-        # Load the Excel file using openpyxl
         excel_data = openpyxl.load_workbook(file_path)
-        # Specify the sheet
         sheet = excel_data['Sheet1']
 
-        # Convert the sheet data to a DataFrame
         data = pd.DataFrame(sheet.values)
 
-        # Assign column names based on the first row
         data.columns = data.iloc[0]
-        data = data[1:]  # Remove the first row
+        data = data[1:]
 
-        # Convert to float data types
         data['Feature1'] = data['Feature1'].astype(float)
         data['Feature2'] = data['Feature2'].astype(float)
         data['Feature3'] = data['Feature3'].astype(float)
@@ -41,7 +33,6 @@ def load_data(file_path):
     except Exception as e:
         return None
 
-# handle user input and make predictions
 def handle_prediction():
     user_input = {
         'Feature1': float(feature1_entry.get()),
@@ -60,18 +51,13 @@ def handle_prediction():
     except Exception as e:
         result_label.config(text=f"Error: {e}")
 
-# Create the main application window
 app = tk.Tk()
 app.title("Biocompatibility Checker")
-
-# Get the screen width and height
 screen_width = app.winfo_screenwidth()
 screen_height = app.winfo_screenheight()
 
-# Set the window size to the screen size
 app.geometry(f"{screen_width}x{screen_height}")
 
-# Add input fields for user to enter features
 feature1_label = tk.Label(app, text="Feature 1:", font=("Helvetica", 20))
 feature1_label.place(x=20,y=10)
 feature1_entry = tk.Entry(app, width= 30, font=("Helvetica", 20))
@@ -87,13 +73,10 @@ feature3_label.place(x=20, y=170)
 feature3_entry = tk.Entry(app, width= 30, font=("Helvetica", 20))
 feature3_entry.place(x=20, y=210)
 
-# Button to trigger prediction
 predict_button = tk.Button(app, text="Predict Biocompatibility", height=5, width= 50, command=handle_prediction)
 predict_button.place(x=20, y=300)
 
-# Label to display the prediction result
 result_label = tk.Label(app, text="", font=("Helvetica", 30))
 result_label.pack()
 
-# Start the Tkinter main loop
 app.mainloop()
