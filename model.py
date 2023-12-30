e3d8af2440a96683c2f1d3dd5af25d26742818a5
RahulPatnaik <142738799+RahulPatnaik@users.noreply.github.com>
2023-12-30 15:42:45 +0530

    Update model.py

--- model.py
+++ model.py
@@ -13,13 +13,11 @@ data = pd.DataFrame({
     'Biocompatible': [0, 0, 1, 1, 0]  #0 for not biocompatible, 1 for biocompatible
 })
 
-# Split the data into features and labels
 features = data[['Feature1', 'Feature2', 'Feature3']]
 labels = data['Biocompatible']
 
 X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
 
-# Create and train a random forest classifier
 model = RandomForestClassifier()
 model.fit(features, labels)
 
@@ -30,7 +28,6 @@ print(f"Model Accuracy: {accuracy*100}%")
 model_path = os.path.abspath('C:\\Users\\Rahul Patnaik\\Desktop\\biocompatibility\\biocompatibility_model.pkl')
 
 try:
-    # Attempt to save the model
     joblib.dump(model, model_path)
     print("Model saved successfully!")
 except Exception as e:
